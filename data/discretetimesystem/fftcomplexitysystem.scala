75f184a450b03bca8bb9c1a12b85a72cdad20a49
Robby McKilliam <robby.mckilliam@gmail.com>
2014-10-28 19:19:42 +1030

    added data

--- data/discretetimesystem/fftcomplexitysystem.scala
+++ data/discretetimesystem/fftcomplexitysystem.scala
@@ -5,27 +5,35 @@ import scala.math.sin
 import scala.math.min
 import scala.math.max
 import scala.math.floor
+import scala.math.round
 import scala.math.ceil
 import scala.math.Pi
 
 println("Generating data for discrete time system modeling the complexity of the Cooley Tukey FFT in Chapter 6")
 
-def lambda(f : Double) = {
-  val e2pif = PolarComplex(1,2*Pi*f)
-  e2pif / (e2pif  - 2)
+val P = 0.5 //period of the discrete time system
+def lambda(alpha : Complex, f : Double) = {
+  val e2pif = PolarComplex(1,2*Pi*P*f)
+  e2pif / (e2pif  - alpha)
 }
-def mod2pi(x : Double) = x - floor(x/2/Pi)*2*Pi
+def mod2pi(x : Double) = x - round(x/2/Pi)*2*Pi
 
-val fmin = 0.00001
-val fmax=3.1
+val fmin = -5.0
+val fmax= 5.0
 
 /// Format Doubles string to a reasonable number of decimal places
 def fmt(x : Double) = "%f" format x
 
-val filetfun = new java.io.FileWriter("datafftcomplexity.csv")
-(fmin to fmax by 0.05) foreach { f =>
-   filetfun.write(fmt(f) + "\t" + fmt(mod2pi(lambda(f).angle)) + "\n")
+val ds = List(2,4,10) //reciprocal of alphas
+for( d <- ds ){
+  val alpha = RectComplex(1.0/d,0)
+  val filetfunabs = new java.io.FileWriter("datafftcomplexityabs" + d + ".csv")
+  val filetfunangle = new java.io.FileWriter("datafftcomplexityangle" + d + ".csv")
+  (fmin to fmax by 0.01) foreach { f =>
+    filetfunabs.write(fmt(f) + "\t" + "\t" + fmt(lambda(alpha,f).magnitude) + "\n")
+    filetfunangle.write(fmt(f) + "\t" + "\t" + fmt(mod2pi(lambda(alpha,f).angle)) + "\n")
+  }
+  filetfunabs.close
+  filetfunangle.close
 }
-filetfun.close
-
 println("Scala finished")
